a38f58892b5f544aa6e4d4d02c903e91e25e921e
Akanov222 <akanov.gazproekt@gmail.com>
2025-09-18 15:24:59 +0300

    Exclude Mapstruct

--- src/main/java/com/example/autosale/controller/CarController.java
+++ src/main/java/com/example/autosale/controller/CarController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -26,6 +27,7 @@ public class CarController {
     private final CarTypeService carTypeService;
     private final CarFactory carFactory;
 
+    @Autowired
     public CarController(SedanRepository sedanRepository,
                          TruckRepository truckRepository,
                          MinivanRepository minivanRepository,
